8cf2266b718b60c21ae0385ec46462d30ea2f93f
Bernard Bruck <bernard.bruck@gmail.com>
2021-08-13 11:31:51 +0200

    Save last changes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks
+  has_many :movies, dependent: :destroy
   has_many :movies, through: :bookmarks
   validates :name, uniqueness: true
+  validates :name, presence: true
 end
